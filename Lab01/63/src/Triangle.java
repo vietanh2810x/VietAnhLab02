b5ef1b6ff1ee19bd907fa8204350a5569ec4ff83
vietanh2810x <khactuananh2810@gmail.com>
2024-11-23 00:54:58 +0700

    Add files via upload

---
+++ Lab01/63/src/Triangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Triangle{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the height of the triangle: ");
+		int n = sc.nextInt();
+		for (int i = 1; i <= n ; i++) {
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0 ; k < 2*i - 1; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
